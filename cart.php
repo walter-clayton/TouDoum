7476116753e12f160dd48ee1a8e0993d9307f87b
Hamza <yajoudhamza@hotmail.com>
2019-11-06 16:52:32 +0100

    + et -

--- cart.php
+++ cart.php
@@ -1,12 +1,9 @@
 <!-- HEADER -->
-<?php
-include('./header.php');
-?>
 
 
 <!DOCTYPE html>
 <html>
-</style>
+
 <head>
 	<title></title>
 </head>
@@ -17,7 +14,14 @@ include('./header.php');
 		<input type="text" id="showamount" value="0">
 		<input type="text" id="priceshow" value="10">
 		<button >ADD</button>	
- <img src="./images/img1.jpg" onclick ="add()">
+ <img src="./images/img1.jpg"> 
+
+ <div>
+ <input type="submit" value="-" onclick = "moins()">
+		<input type="submit" value="+" onclick ="plus()">
+		
+		
+	</div>
 	</div>
 	<div>
 	<select name="Country" id="lol" >
@@ -25,17 +29,13 @@ include('./header.php');
   <option value="2.5" id="eu">EU +2.5</option> 
   <option value="5" id="other">OTHER +5</option> 
 	</select>
-
 	</div>
 	<div>
 		<input type="text" name="promo" placeholder="PROMO" id="promo">
-
 	</div>	
 	<div>
 		<input type="submit" name="submit" onclick="promo()">
 	</div>
-
-
 		<h1 ><span>	TOTAL PRICE </span><p id="pricetotal" ></p></h1><hr>
 		<h2>REDUCTION <span id = "reductionresult"></span></h2><hr>
 		<h2>DELIVERY <span id = "deliveryresult"></span></h2><hr>
@@ -43,94 +43,99 @@ include('./header.php');
 	
 <div>
 </div>
-
-
-
-
-
 <script type="text/javascript">
-
 const select = document.getElementById("lol");
+	function plus() {
+	var val = parseInt(document.getElementById("showamount").value);
 
-	function add() {
+	val++ ;
+
+
+
+var priceshow = parseInt(document.getElementById("priceshow").value);
+console.log(priceshow);
+	price = 10;
+var pricetotal = price * val;
+console.log(pricetotal)
+if (val >= 5) {
+		var reduction =  (pricetotal - (pricetotal * 5/100)) + " -5% " ;
+		console.log(reduction)
+		document.getElementById("reductionresult").value = reduction;
+		document.getElementById("reductionresult").innerHTML = reduction;
+		}
+	var priceshow = pricetotal;
+document.getElementById("pricetotal").innerHTML = pricetotal;
+document.getElementById("showamount").value = val;
+document.getElementById("priceshow").innerHTML = priceshow;
+	}
+
+
+
+
+	function moins() {
 var val = parseInt(document.getElementById("showamount").value);
-console.log(val);
-	val++
+
+	val-- ;
+
+
+
 var priceshow = parseInt(document.getElementById("priceshow").value);
 console.log(priceshow);
 	price = 10;
 var pricetotal = price * val;
 console.log(pricetotal)
 
+
 if (val >= 5) {
-
-		var reduction =  (pricetotal - (pricetotal * 5/100))+ " you get 5% reduction " ;
+		var reduction =  (pricetotal - (pricetotal * 5/100)) + " -5% "  ;
 		console.log(reduction)
-		document.getElementById("reductionresult").value = reduction;
-		}
 
+		
+		document.getElementById("reductionresult").value = reduction;
+		document.getElementById("reductionresult").innerHTML = reduction;
+		
+		} else if (val < 5){
+		reduction = 0;
+	
+		document.getElementById("reductionresult").value = reduction;
+		document.getElementById("reductionresult").innerHTML = reduction;
+		}
 
 	var priceshow = pricetotal;
 document.getElementById("pricetotal").innerHTML = pricetotal;
 document.getElementById("showamount").value = val;
 document.getElementById("priceshow").innerHTML = priceshow;
-document.getElementById("reductionresult").innerHTML = reduction;
-
 	}
-
-
-
 select.onchange = function() {
-
 var  pricetotalCountry = document.getElementById("pricetotal").innerHTML;
-
-
-	var belgium = document.getElementById("belgium").value;
-var addition = (pricetotalCountry + currentoption) + " FREE DELIVERY";
-
-var eu = document.getElementById("eu").value;
-var addition = (pricetotalCountry + currentoption) + " $2.5 FEE DELIVERY";
-
-var other = document.getElementById("other").value;
-var addition = (pricetotalCountry + currentoption) + " $5 FEE DELIVERY";
-
-
-document.getElementById("deliveryresult").innerHTML = currentoption;
-
 var currentoption = select.options[select.selectedIndex].value;
 console.log(currentoption);
-
-
-
+document.getElementById("deliveryresult").innerHTML = currentoption;
+if (currentoption === "0") {
+	var addition = " FREE";
+}
+else if (currentoption === "2.5"){
+	var addition = " +$2.5";
+}
+else {
+var addition = " +$5";
+}
+document.getElementById("deliveryresult").innerHTML = addition;
 	}
-
-
 		function promo() {
-
-
 		var promo = document.getElementById("promo").value;
 		console.log(promo)
-
-
 			if ('MikeEstTropCool' == promo) {
 				console.log("you get 10% reduction")
 			document.getElementById("promoresult").innerHTML = "you get 10% reduction";
-
 			}
-
 			else {
 			console.log("wrong PROMOCODE")
-			document.getElementById("promoresult").innerHTML = "wrong PROMOCODE";
-
+			document.getElementById("promoresult").innerHTML = "WRONG CODE";
 			}
 	}
 
+
 </script>
-
-
-
-
-
-
 </body>
 </html>
